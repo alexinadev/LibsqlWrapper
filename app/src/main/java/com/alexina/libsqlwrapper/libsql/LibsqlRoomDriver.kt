8f05699163078cd85f1c5b71a5813524ca53f8fd
alexina <khorshidian_sina@yahoo.com>
2025-03-09 10:38:11 +0330

    import as a aar

--- app/src/main/java/com/alexina/libsqlwrapper/libsql/LibsqlRoomDriver.kt
+++ app/src/main/java/com/alexina/libsqlwrapper/libsql/LibsqlRoomDriver.kt
@@ -1,20 +1,10 @@
 package com.alexina.libsqlwrapper.libsql
 
-import android.content.ContentValues
 import android.content.Context
-import android.database.Cursor
-import android.database.sqlite.SQLiteTransactionListener
-import android.os.CancellationSignal
-import android.util.Pair
 import androidx.sqlite.db.SupportSQLiteDatabase
 import androidx.sqlite.db.SupportSQLiteOpenHelper
-import androidx.sqlite.db.SupportSQLiteQuery
-import androidx.sqlite.db.SupportSQLiteStatement
 import tech.turso.libsql.EmbeddedReplicaDatabase
 import tech.turso.libsql.Libsql
-import java.util.Locale
-import kotlin.Throws
-import kotlin.jvm.Throws
 
 class LibsqlRoomDriver(context: Context) : SupportSQLiteOpenHelper {
     val dbPath = context.getDatabasePath("local.db").absolutePath // Use app's private database directory,
